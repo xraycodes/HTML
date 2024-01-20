c87b885854eb13dcf2f06786a3d75c71c5a44646
brianjo94 <brianjo1994@gmail.com>
2024-01-20 13:20:45 -0500

    callback functions, using error and data

--- nodeJS/starter/index.js
+++ nodeJS/starter/index.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 
-// To read files
-const textIn = fs.readFileSync('./txt/final.txt', 'utf-8');
-console.log(textIn);
+//Blocking, synchronous way
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
-fs.writeFileSync('./txt/output.txt', textOut)
-console.log('File written')
+// To read files
+// const textIn = fs.readFileSync('./txt/final.txt', 'utf-8');
+// console.log(textIn);
+//To write files
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut)
+// console.log('File written')
+
+// Non-blocking, asynchronous way
+fs.readFile('./txt/starts.txt', 'utf-8', (err, data)=> {
+    if (err) return console.log('Error')
+    console.log(data)
+})
+console.log("Will read file!")
