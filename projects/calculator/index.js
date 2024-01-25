fed3fccb8caa18dce9877a9f4193cd13e5791d3f
brianjo94 <brianjo1994@gmail.com>
2024-01-24 23:09:16 -0500

    working on iphone calculator

--- projects/calculator/index.js
+++ projects/calculator/index.js
@@ -1,2 +1,23 @@
+let output = ""
+
+
 const answer = document.querySelector(".answer")
-console.log(answer)
+const clear = document.querySelector(".clear")
+
+const seven = document.querySelector(".seven")
+
+let numbers = ["one","two","three","four","five","six","seven","eight","nine"]
+
+clear.addEventListener("click", ()=>{
+    output = 0
+    answer.textContent = output
+})
+
+for (let i=0; i < numbers.length; i++) {
+    console.log(typeof(Number(document.querySelector(`.${numbers[i]}`).textContent)))
+    console.log(answer.textContent)
+    document.querySelector(`.${numbers[i]}`).addEventListener("click", ()=>{
+        let result = Number(answer.textContent) + Number(document.querySelector(`.${numbers[i]}`).textContent)
+        answer.textContent = result
+    })
+}
