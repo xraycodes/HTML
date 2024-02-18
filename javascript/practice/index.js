534ae53c486dc5849cafa12df3aa2dc193cd4a74
brianjo94 <brianjo1994@gmail.com>
2024-02-18 17:50:57 -0500

    using DOM for radio/checkboxes, using .checked to access if the inputs were clicked or not

--- javascript/practice/index.js
+++ javascript/practice/index.js
@@ -1,22 +1,25 @@
-let age = 16;
-let license = false;
+const subscribed = document.querySelector("#subscribe")
+const visa = document.querySelector("#visa")
+const para = document.querySelector("#sub")
+const para2 = document.querySelector("#sub2")
+const button = document.querySelector("#btn")
 
 
-myText = document.querySelector("#myText")
-mySubmit = document.querySelector("#mySubmit")
-resultElement = document.querySelector("#resultElement")
 
-mySubmit.addEventListener("click", () => {
-    age = Number(myText.value)
-    if (age> 18) {
-        resultElement.textContent = "You may enter website"
-    }
-    else if (age === 0 || isNaN(age)) {
-        resultElement.textContent = "You can't be 0 years old or enter valid number"
+button.addEventListener("click", () => {
+    if (subscribed.checked) {
+        para.innerHTML = "You are subscribed"
     }
     else {
-        resultElement.textContent = "You are too young to enter website"
+        para.innerHTML = "You are not subscribed"
     }
 })
 
-console.log(myText.value)
+button.addEventListener("click", () => {
+    if (visa.checked) {
+        para2.innerHTML = "You are using visa"
+    }
+    else {
+        para2.innerHTML = "You are not using visa"
+    }
+})
