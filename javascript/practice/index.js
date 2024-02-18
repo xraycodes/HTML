797ad84ddf1f17451b8eea38dc2f40488999f2f0
brianjo94 <brianjo1994@gmail.com>
2024-02-18 17:44:47 -0500

    are you older than this age check

--- javascript/practice/index.js
+++ javascript/practice/index.js
@@ -1,9 +1,22 @@
-let randomNumber = 0
+let age = 16;
+let license = false;
 
-document.querySelector("#buttons").addEventListener("click", function(){
-    // console.log(randomNumber)
-    randomNumber = Math.floor(Math.random() * 101)
-    document.querySelector("#random-number").textContent = randomNumber
+
+myText = document.querySelector("#myText")
+mySubmit = document.querySelector("#mySubmit")
+resultElement = document.querySelector("#resultElement")
+
+mySubmit.addEventListener("click", () => {
+    age = Number(myText.value)
+    if (age> 18) {
+        resultElement.textContent = "You may enter website"
+    }
+    else if (age === 0 || isNaN(age)) {
+        resultElement.textContent = "You can't be 0 years old or enter valid number"
+    }
+    else {
+        resultElement.textContent = "You are too young to enter website"
+    }
 })
 
-
+console.log(myText.value)
