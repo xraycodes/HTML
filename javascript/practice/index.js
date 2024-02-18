7e141e7383923b5e5ac212d11f7d952d4094b969
brianjo94 <brianjo1994@gmail.com>
2024-02-18 18:00:02 -0500

    using ternary operator(if/else)

--- javascript/practice/index.js
+++ javascript/practice/index.js
@@ -1,25 +1,6 @@
-const subscribed = document.querySelector("#subscribe")
-const visa = document.querySelector("#visa")
-const para = document.querySelector("#sub")
-const para2 = document.querySelector("#sub2")
-const button = document.querySelector("#btn")
+let age = 15
 
+// Ternary operator
+const message = age >= 18 ? "You are an adult" : "You are a minor"
 
-
-button.addEventListener("click", () => {
-    if (subscribed.checked) {
-        para.innerHTML = "You are subscribed"
-    }
-    else {
-        para.innerHTML = "You are not subscribed"
-    }
-})
-
-button.addEventListener("click", () => {
-    if (visa.checked) {
-        para2.innerHTML = "You are using visa"
-    }
-    else {
-        para2.innerHTML = "You are not using visa"
-    }
-})
+console.log(message)
